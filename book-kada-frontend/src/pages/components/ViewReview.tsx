135ca8c0c0d5d26b6d1e0c2a403e011680eda713
EliasThambi <eliasthambi@gmail.com>
2022-11-22 18:45:13 +0530

    Update on Review Card

---
+++ book-kada-frontend/src/pages/components/ViewReview.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { FC, useState } from "react";
+
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea, Rating, Button, Typography } from "@mui/material";
+import CentreBox from "./centerBox";
+import LeftBox from "./leftBox";
+
+export interface IAppProps {}
+
+export default function ViewReview(props: IAppProps) {
+  return (
+    <div>
+      <Card
+        sx={{
+          width: 1,
+          display: "grid",
+          gap: 1,
+          gridTemplateColumns: "repeat(3, 1fr)",
+        }}
+      >
+        <CardContent>Name of User</CardContent>
+
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Rating :{" "}
+            <Rating
+              name="half-rating"
+              defaultValue={2.5}
+              precision={0.5}
+              readOnly
+            />
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- book-kada-frontend/src/pages/details.tsx
+++ book-kada-frontend/src/pages/details.tsx
@@ -1,37 +1,22 @@
 import { FC, useState } from "react";
 
-import React from "react";
-import Box, { BoxProps } from "@mui/material/Box";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea, Rating, Button, TextField, Grid } from "@mui/material";
+import { CardActionArea, Rating, Button, Typography } from "@mui/material";
 import CentreBox from "./components/centerBox";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import Padding from "./components/padding";
 import LeftBox from "./components/leftBox";
+import Review from "./components/AddReview";
+import ViewReview from "./components/ViewReview";
 
 interface DetailsProps {}
 
 const Details: FC<DetailsProps> = ({}) => {
   const [addflag, setAddflag] = useState(false);
-  const [viewflag, setViewflag] = useState(false);
-  const [ratingvalue, setRatingValue] = useState(0);
-  const [reviewdata, setReviewData] = useState('');
-  
 
-
-  function addReview() {
-    setAddflag(!addflag);
-  }
-  function viewReview() {
-    setViewflag(!viewflag);
-  }
-  function reviewSubmit(){
-    setAddflag(!addflag);
-
-  }
   return (
     <Padding>
       <Card
@@ -46,13 +31,13 @@ const Details: FC<DetailsProps> = ({}) => {
           <CardMedia
             component="img"
             height="500"
-            image="/static/images/cards/contemplative-reptile.jpg"
+            image="https://c1.wallpaperflare.com/preview/127/366/443/library-book-bookshelf-read.jpg"
             alt="green iguana"
           />
         </CardActionArea>
-        
+
         <CardContent>
-       <LeftBox>
+          <LeftBox>
             <Typography gutterBottom variant="h5" component="div">
               Rating :{" "}
               <Rating
@@ -73,12 +58,6 @@ const Details: FC<DetailsProps> = ({}) => {
 
           <CentreBox>
             <Box sx={{ display: "flex", gap: 5 }}>
-              <Button variant="contained" color="success" onClick={addReview}>
-                Add Review
-              </Button>
-              <Button variant="contained" color="success" onClick={viewReview}>
-                View Review
-              </Button>
               <Button
                 variant="contained"
                 color="success"
@@ -89,27 +68,12 @@ const Details: FC<DetailsProps> = ({}) => {
             </Box>
           </CentreBox>
 
-          {addflag && (
-            <Typography
-              gutterBottom
-              variant="h6"
-              component="div"
-              sx={{ display: "flex", gap: 3 }}
-            >
-              Review : <TextField variant="outlined" size="small" onChange={(e)=>setReviewData(e.target.value)}/>
-              Rating :{" "}
-              <Rating name="half-rating" defaultValue={2.5} precision={0.5} onChange={(event, newRatingValue) => {setRatingValue(ratingvalue)}}/>
-              <Button variant="contained" color="success" onClick={reviewSubmit}>
-                Submit
-              </Button>
-            </Typography>
-          )}
+          <Review setAddflag={setAddflag} addflag={addflag} />
         </CardContent>
       </Card>
 
-      {viewflag&&
       <h1>Reviews</h1>
-      }
+      <ViewReview />
     </Padding>
   );
 };
