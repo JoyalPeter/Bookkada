5bb02dd8d7f466921eab45a2136da69b1bd740f4
EliasThambi <eliasthambi@gmail.com>
2022-11-24 18:14:16 +0530

    Routing Params Updated

--- book-kada-frontend/src/BookDetails/DetailsCard.tsx
+++ book-kada-frontend/src/BookDetails/DetailsCard.tsx
@@ -1,19 +1,19 @@
 import { FC, useEffect, useState } from "react";
 
-
 import CentreBox from "../UI/CenterBox";
 import Padding from "../UI/Padding";
 import ViewReview from "../BookDetails/ViewReview";
-import DetailsSubCard from './DetailsSubCard'
+import DetailsSubCard from "./DetailsSubCard";
 import { Method } from "../constants/enums";
 import useApiService from "../hooks/UseApiService";
 import Spinner from "../UI/Spinner";
+import { useParams } from "react-router-dom";
 
 interface DetailsProps {}
 
-export interface RatingInterface{
-  description:string;
-  rating:number
+export interface RatingInterface {
+  description: string;
+  rating: number;
 }
 
 export interface BookDetails {
@@ -27,28 +27,33 @@ export interface BookDetails {
 const DetailsCard: FC<DetailsProps> = ({}) => {
   const [resp, setResponse] = useState([] as BookDetails[]);
   const { makeApiCall, loadingFlag } = useApiService();
-
+  const { id } = useParams();
   useEffect(() => {
-    makeApiCall(Method.GET, "books/getBook/2")
+    makeApiCall(Method.GET, `books/getBook/${id}`)
       .then((response: BookDetails[]) => {
-        console.log("details card", response);
+        console.log("details", response);
 
         setResponse(response);
       })
       .catch((error) => error);
   }, []);
   console.log(loadingFlag);
-  
+
   return (
     <>
-    <Padding>
-      {loadingFlag ? (
-        <Spinner />
-      ) :(<><DetailsSubCard resp={resp}/><CentreBox>
-            <h1>Reviews</h1>
-          </CentreBox><ViewReview /></>)
-      }
-    </Padding>
+      <Padding>
+        {loadingFlag ? (
+          <Spinner />
+        ) : (
+          <>
+            <DetailsSubCard resp={resp} />
+            <CentreBox>
+              <h1>Reviews</h1>
+            </CentreBox>
+            <ViewReview />
+          </>
+        )}
+      </Padding>
     </>
   );
 };
