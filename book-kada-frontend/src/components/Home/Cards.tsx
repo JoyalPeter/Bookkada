963583acb3d0d4e2ded3edde312a77cdc5736670
ann-maria-shaji <annmariashaji146@gmail.com>
2022-11-24 18:05:23 +0530

    styling updations done

--- book-kada-frontend/src/components/Home/Cards.tsx
+++ book-kada-frontend/src/components/Home/Cards.tsx
@@ -6,8 +6,6 @@ import {
   CardContent,
   CardActions,
   Typography,
-  ButtonBase,
-  useMediaQuery,
 } from "@mui/material";
 import Cart from "./Shoppingcart";
 import Favorites from "./Addfavorite";
@@ -18,65 +16,47 @@ import Spinner from "../../UI/Spinner";
 import showToast from "../../utils/Toastify";
 import { BookDataProps } from "./HomeComponent";
 
+
 export default function Cards(props: BookDataProps) {
   const navigate = useNavigate();
-  const { makeApiCall, loadingFlag } = useApiService();
-
-  useEffect(() => {
-    const displayBooks = () => {
-      makeApiCall(Method.GET, "books/viewAllbooks")
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => showToast(Toast.ERROR, error));
-    };
-    displayBooks();
-  });
-
   return (
     <>
-      {loadingFlag ? (
-        <Spinner />
-      ) : (
-        <Card sx={{ maxWidth: 275, boxShadow: 5 }}>
-          <ButtonBase
-            onClick={() => navigate(`details/:id`)}
-            // console.log("kitti")
-          ></ButtonBase>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://c1.wallpaperflare.com/preview/127/366/443/library-book-bookshelf-read.jpg"
-            alt="book1"
-          />
-          <CardContent>
-            <>
-              <Typography gutterBottom variant="h5" component="div">
-                <b>{props.name}</b>
-              </Typography>
-              <Typography gutterBottom variant="subtitle1" component="div">
-                <b>{props.author}</b>
-              </Typography>
-              <Typography variant="body1" color="text.secondary" fontSize={16}>
-                <b>$</b>
-                {props.price}
-              </Typography>
-              <Typography
-                fontSize={14}
-                fontWeight={75}
-                fontFamily={"Arial"}
-                variant="body2"
-                color="text.secondary"
-              >
-                <b>{props.description}</b>
-              </Typography>
-            </>
-          </CardContent>
-          <CardActions>
-            <Cart /> <Favorites /> <Ratings />
-          </CardActions>
-        </Card>
-      )}
+       
+      <Card sx={{ maxWidth: 275, boxShadow: 5, m: 1, maxHeight: 500 }}>
+        <CardMedia
+          component="img"
+          height="150"
+          image="https://c1.wallpaperflare.com/preview/127/366/443/library-book-bookshelf-read.jpg"
+          alt="name"
+        />
+        <CardContent onClick={() => navigate(`details`)}>
+          <>
+            <Typography gutterBottom variant="h5" component="div">
+              <b>{props.name}</b>
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              <b>{props.author}</b>
+            </Typography>
+            <Typography variant="body1" color="text.secondary" fontSize={16}>
+              <b>$</b>
+              {props.price}
+            </Typography>
+            <Typography
+              fontFamily={"monospace"}
+              fontWeight={"light"}
+              variant="subtitle2"
+              color="text.secondary"
+              component="div"
+            >
+              {props.description}
+            </Typography>
+          </>
+        </CardContent>
+        <CardActions>
+          <Cart /> <Favorites /> <Ratings />
+        </CardActions>
+      </Card>
+       
     </>
   );
 }
