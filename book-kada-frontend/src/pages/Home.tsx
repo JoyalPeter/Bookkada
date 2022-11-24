659785ea4d2dabc33ea2066ba579b701019a9eab
abhijithcarestack <abhijithss@carestack.com>
2022-11-24 22:11:38 +0530

    vf

--- book-kada-frontend/src/pages/Home.tsx
+++ book-kada-frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import UserPage from '../components/home/HomeComponent';
+import React from "react";
+import UserPage from "../components/Home/HomeComponent";
 
-import AppBar from '../UI/AppBar/AppBar';
+import AppBar from "../UI/AppBar/AppBar";
 
 export default function Home() {
   return (
