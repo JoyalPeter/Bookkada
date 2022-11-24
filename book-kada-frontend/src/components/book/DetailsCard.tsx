a54dd7624f25cfb65b3ed57b01964bf83ada1055
abhijithcarestack <abhijithss@carestack.com>
2022-11-24 19:48:41 +0530

    changed filenames

--- book-kada-frontend/src/components/book/DetailsCard.tsx
+++ book-kada-frontend/src/components/book/DetailsCard.tsx
@@ -1,21 +1,57 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import CentreBox from '../../UI/CenterBox';
 import Padding from '../../UI/Padding';
 import ViewReview from './ViewReview';
 import DetailsSubCard from './DetailsSubCard';
+import { Method } from '../../constants/Enums';
+import useApiService from '../../hooks/UseApiService';
+import Spinner from '../../UI/Spinner';
 
 interface DetailsProps {}
 
+export interface RatingInterface {
+  description: string;
+  rating: number;
+}
+
+export interface BookDetails {
+  name: string;
+  price: string;
+  description: string;
+  author: string;
+  ratings: RatingInterface[];
+}
+
 const DetailsCard: FC<DetailsProps> = ({}) => {
+  const [resp, setResponse] = useState([] as BookDetails[]);
+  const { makeApiCall, loadingFlag } = useApiService();
+
+  useEffect(() => {
+    makeApiCall(Method.GET, 'books/getBook/2')
+      .then((response: BookDetails[]) => {
+        console.log('details card', response);
+
+        setResponse(response);
+      })
+      .catch((error) => error);
+  }, []);
+  console.log(loadingFlag);
+
   return (
     <>
       <Padding>
-        <DetailsSubCard />
-        <CentreBox>
-          <h1>Reviews</h1>
-        </CentreBox>
-        <ViewReview />
+        {loadingFlag ? (
+          <Spinner />
+        ) : (
+          <>
+            <DetailsSubCard resp={resp} />
+            <CentreBox>
+              <h1>Reviews</h1>
+            </CentreBox>
+            <ViewReview />
+          </>
+        )}
       </Padding>
     </>
   );
