18a0474c64963e55f1880da912d45908c32654db
ann-maria-shaji <annmariashaji146@gmail.com>
2022-11-25 20:56:54 +0530

    detailscard edits

--- book-kada-frontend/src/components/book/DetailsSubCard.tsx
+++ book-kada-frontend/src/components/book/DetailsSubCard.tsx
@@ -12,7 +12,7 @@ import Review from "./AddReview";
 import { BookDetails } from "./DetailsCard";
 
 interface DetailsProps {
-  resp: BookDetails[];
+  resp: BookDetails | null;
 }
 
 export default function DetailsSubCard({ resp }: DetailsProps) {
@@ -23,81 +23,72 @@ export default function DetailsSubCard({ resp }: DetailsProps) {
   function addReview() {
     setaddReviewFlag(!addReviewFlag);
   }
-  if (resp[0]) {
+  console.log(resp);
+  if (resp) {
     //  console.log("rating", resp.ratings);
-    totalNumberOfReview = resp[0].ratings.length;
-    resp[0].ratings.map((item) => (reviewSum = reviewSum + item.rating));
+    totalNumberOfReview = resp.ratings.length;
+    resp.ratings.map((item) => (reviewSum = reviewSum + item.rating));
     reviewAvg = reviewSum / totalNumberOfReview;
   }
   return (
-    <div>
-      {resp.map(
-        (item: BookDetails): JSX.Element => (
-          <Card
-            sx={{
-              width: 1,
-              display: "grid",
-              gap: 1,
-              gridTemplateColumns: "repeat(2, 1fr)",
-            }}
-          >
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="500"
-                image="https://c1.wallpaperflare.com/preview/127/366/443/library-book-bookshelf-read.jpg"
-                alt="green iguana"
-              />
-            </CardActionArea>
+    <Card
+      sx={{
+        width: 1,
+        display: "grid",
+        gap: 1,
+        gridTemplateColumns: "repeat(2, 1fr)",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="500"
+          image="https://c1.wallpaperflare.com/preview/127/366/443/library-book-bookshelf-read.jpg"
+          alt="green iguana"
+        />
+      </CardActionArea>
 
-            <CardContent>
-              <LeftBox>
-                <Typography gutterBottom variant="h5" component="div">
-                  Rating :{" "}
-                  <Rating
-                    name="half-rating"
-                    defaultValue={item.ratings[0].rating}
-                    precision={0.5}
-                    readOnly
-                  />
-                </Typography>
-              </LeftBox>
-              <Typography gutterBottom variant="h3" component="div">
-                {/* {resp[0] && resp[0].name} */}
-                {item.name}
-              </Typography>
-              <Typography gutterBottom variant="h6" component="div">
-                {/* {resp[0] && resp[0].name} */}
-
-                {item.author}
-              </Typography>
-              <Typography gutterBottom variant="h5" component="div">
-                {/* {resp[0] && resp[0].name} */}${item.price}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {/* {resp[0] && resp[0].description} */}
-                {item.description}
-              </Typography>
-              <CentreBox>
-                <Box sx={{ display: "flex", gap: 5 }}>
-                  <Button variant="contained" onClick={addReview}>
-                    Add Review
-                  </Button>
-                  <Button variant="contained" endIcon={<AddShoppingCartIcon />}>
-                    Add To Cart
-                  </Button>
-                </Box>
-              </CentreBox>
-              {addReviewFlag && (
-                <Review
-                  addReviewFlag={addReviewFlag}
-                  setaddReviewFlag={setaddReviewFlag}
-                />
-              )}
-            </CardContent>
-          </Card>
-        )
-      )}
-    </div>
+      <CardContent>
+        <LeftBox>
+          <Typography gutterBottom variant="h5" component="div">
+            Rating :{" "}
+            <Rating
+              name="half-rating"
+              defaultValue={resp?.ratings[0].rating}
+              precision={0.5}
+              readOnly
+            />
+          </Typography>
+        </LeftBox>
+        <Typography gutterBottom variant="h3" component="div">
+          {resp?.name}
+        </Typography>
+        <Typography gutterBottom variant="h6" component="div">
+          {resp?.author}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          ${resp?.price}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {resp?.description}
+        </Typography>
+        <CentreBox>
+          <Box sx={{ display: "flex", gap: 5 }}>
+            <Button variant="contained" onClick={addReview}>
+              Add Review
+            </Button>
+            <Button variant="contained" endIcon={<AddShoppingCartIcon />}>
+              Add To Cart
+            </Button>
+          </Box>
+        </CentreBox>
+        {addReviewFlag && (
+          <Review
+            addReviewFlag={addReviewFlag}
+            setaddReviewFlag={setaddReviewFlag}
+          />
+        )}
+      </CardContent>
+    </Card>
   );
 }
