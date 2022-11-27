a50a9ff48a1d180e7a6628ec7b0fcfb62db5241e
EliasThambi <eliasthambi@gmail.com>
2022-11-27 20:38:02 +0530

    Refactoring Profile

--- book-kada-frontend/src/components/profile/ProfileModule.tsx
+++ book-kada-frontend/src/components/profile/ProfileModule.tsx
@@ -1,7 +1,6 @@
 import Padding from "../../UI/Padding";
 import {
   Avatar,
-  Box,
   Card,
   Container,
   Grid,
@@ -29,7 +28,9 @@ export default function ProfileModule() {
   const usercontext = useContext(UserContext);
   const [response, setResponse] = useState<UserDetails | null>(null);
   const { makeApiCall, loadingFlag } = useApiService();
-  const [updateflag, setupdateflag] = useState(false);
+  const [updatenameflag, setNameflag] = useState(true);
+  const [updatePasswordflag, setPasswordflag] = useState(true);
+
   const [newName, setNewName] = useState("");
   const [newPassword, setnewPassword] = useState("");
   const userid = usercontext?.userDetails.userId;
@@ -38,13 +39,22 @@ export default function ProfileModule() {
       setResponse(response);
     });
   }, []);
-  function updateUserProfile() {
-    setupdateflag(!updateflag);
+  function updateName() {
+    setNameflag(!updatenameflag);
   }
-  function userProfileUpdated() {
+  function updatePassword() {
+    setPasswordflag(!updatePasswordflag);
+  }
+  function userNameUpdated() {
+    setNameflag(!updatenameflag);
     makeApiCall(Method.PATCH, `users/updateUser/${userid}`, {
-      name: newName,
-      password: newPassword,
+      name: newName
+    }).catch((error) => showToast(Toast.ERROR, error));
+  }
+  function userPasswordUpdated() {
+    setPasswordflag(!updatePasswordflag);
+    makeApiCall(Method.PATCH, `users/updateUser/${userid}`, {
+      password: newPassword
     }).catch((error) => showToast(Toast.ERROR, error));
   }
 
@@ -69,37 +79,56 @@ export default function ProfileModule() {
                 <Typography component="h1" variant="h5">
                   Profile
                 </Typography>
-                <Typography component="h1" variant="h5">
-                  Name : {response.name}
-                </Typography>
-                <Typography component="h1" variant="h5">
-                  Email : {response.email}
-                </Typography>
-                <Grid container justifyContent="center">
-                  <IconButton
-                    onClick={updateUserProfile}
-                    sx={{ mt: 3, mb: 2 }}
-                    aria-label="delete"
-                  >
-                    <EditIcon />
-                  </IconButton>
+                {updatenameflag ? (
+                  <>
+                    <Typography component="h1" variant="h5">
+                      Name : {response.name}
+                      <IconButton
+                        onClick={updateName}
+                        sx={{ mt: 3, mb: 3 }}
+                        aria-label="delete"
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Typography>
+                  </>
+                ) : (
+                  <>
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstname"
+                      required
+                      fullWidth
+                      id="first name"
+                      label={`${response.name}`}
+                      autoFocus
+                      onChange={(e) => setNewName(e.target.value)}
+                    />
+                    <IconButton onClick={userNameUpdated}>
+                      <BuildIcon />
+                    </IconButton>
+                  </>
+                )}
+                <Grid>
+                  <Typography component="h1" variant="h5">
+                    Email : {response.email}
+                  </Typography>
                 </Grid>
-                {updateflag && (
-                  <Box>
-                    <Grid container spacing={2}>
-                      <Grid item xs={12}>
-                        <TextField
-                          autoComplete="given-name"
-                          name="firstname"
-                          required
-                          fullWidth
-                          id="first name"
-                          label={`${response.name}`}
-                          autoFocus
-                          onChange={(e) => setNewName(e.target.value)}
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
+                {updatePasswordflag ? (
+                  <>
+                    <Typography component="h1" variant="h5">
+                      Password{" "}
+                      <IconButton
+                        onClick={updatePassword}
+                        sx={{ mt: 3, mb: 3 }}
+                        aria-label="delete"
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Typography>
+                  </>
+                ) : (
+                  <>
                         <TextField
                           required
                           fullWidth
@@ -109,31 +138,10 @@ export default function ProfileModule() {
                           autoComplete="email"
                           onChange={(e) => setnewPassword(e.target.value)}
                         />
-                      </Grid>
-                      {/* <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        name="password"
-                        label={``}
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                      />
-                    </Grid> */}
-                      <Grid item xs={12} container justifyContent="center">
-                        <IconButton onClick={userProfileUpdated}>
+                        <IconButton onClick={userPasswordUpdated}>
                           <BuildIcon />
                         </IconButton>
-                      </Grid>
-                    </Grid>
-
-                    {/* <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained" 
-                > */}
-                  </Box>
+                  </>
                 )}
               </CenterBox>
             </Container>
