03f6f9b4e1014a4e88987b325ef615aa6598a5bf
abhijithcarestack <abhijithss@carestack.com>
2022-11-27 21:49:51 +0530

    changes in ui

--- book-kada-frontend/src/pages/Profile.tsx
+++ book-kada-frontend/src/pages/Profile.tsx
@@ -1,8 +1,10 @@
 import ProfileModule from "../components/profile/ProfileModule";
+import PrimarySearchAppBar from "../UI/AppBar/AppBar";
 
 export default function ProfilePage() {
   return (
     <div>
+      <PrimarySearchAppBar />
       <ProfileModule />
     </div>
   );
